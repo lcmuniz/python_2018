4e43976e383ca6f4a0a3410d3841bfd4d8ac5c1e
Luiz Carlos Muniz <lcmuniz@gmail.com>
2018-11-13 17:17:02 -0300

    Update aluno.py

--- aluno.py
+++ aluno.py
@@ -3,10 +3,13 @@ class Aluno:
     def __init__(self):
         self.matricula = ""
         self.nome = ""
-        self.nota1 = ""
-        self.faltas1 = ""
-        self.nota2 = ""
-        self.faltas2 = ""
+        self.nota1 = 0
+        self.faltas1 = 0
+        self.nota2 = 0
+        self.faltas2 = 0
 
     def __str__(self):
-        return self.matricula + " - " + self.nome
+        media = (self.nota1 + self.nota2) / 2
+        faltas = self.faltas1 + self.faltas2
+        return 'Matrícula: ' + self.matricula + ", Nome: " + self.nome + \
+               ', Média: ' + str(media) + ', Faltas: ' + str(faltas)
